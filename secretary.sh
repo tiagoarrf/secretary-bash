534dbf86cb62aa85ebdd1ca559997d6d49084c07
tiagoarrfon <104157123+tiagoarrfon@users.noreply.github.com>
2022-11-03 15:32:20 -0300

    numbers files info modev
    numbers files info modev

--- secretary.sh
+++ secretary.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 #By Tiago A. Fontenele
-mkdir other 2> /dev/null
-mv `ls -p | grep -v / | grep  -v "\."` other/ 2> /dev/null
-mv `ls -p | grep -v / | grep -e ".*\.$"` other/ 2> /dev/null
-mkdir -p `ls -p | grep -v / | grep -o  "[^.]*$" | tr [:upper:] [:lower:]` 2> /dev/null
-ls -p | grep -v / | grep  "\." > tempirj0f49j49r
-while read LIN
-   do
-      mv "$LIN" "$(echo "$LIN" | grep -v / | grep "\." | grep -o  "[^.]*$" | tr [:upper:] [:lower:])/" 2> /dev/null
-done < tempirj0f49j49r
-rm tempirj0f49j49r
-mv sh/secretary.sh secretary.sh 2> /dev/null
-echo Finished.
+mkdir other 2>/dev/null
+mv $(ls -p | grep -v / | grep -v "\.") other/ 2>/dev/null
+mv $(ls -p | grep -v / | grep -e ".*\.$") other/ 2>/dev/null
+mkdir -p $(ls -p | grep -v / | grep -o "[^.]*$" | tr [:upper:] [:lower:]) 2>/dev/null
+num_files=$(ls -p | grep -v / | grep "\." >f0exc0xa0xe0x0x8a9 | cat f0exc0xa0xe0x0x8a9 | wc -l)
+i=0
+while read LIN; do
+      ((i = i + 1))
+      echo " Moveing $i of $num_files files"
+      mv "$LIN" "$(echo "$LIN" | grep -v / | grep "\." | grep -o "[^.]*$" | tr [:upper:] [:lower:])/" 2>/dev/null
+      clear
+done <f0exc0xa0xe0x0x8a9
+rm f0exc0xa0xe0x0x8a9
+cp sh/secretary.sh secretary.sh 2>/dev/null
+echo " Process finished!"
+echo " $i files were organized into folders."
+sleep 5
+exit
